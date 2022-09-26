cd88724c261ee7d107da458a5ebaf7ba0d518743
saurabh-cognologix <saurabh.singh@cognologix.com>
2022-09-26 16:48:48 +0530

    refactored

--- assignmentset2/question1/mapper/ServerMapper.java
+++ assignmentset2/question1/mapper/ServerMapper.java
@@ -1,15 +1,28 @@
 package corejava.collection.assignmentset2.question1.mapper;
 
 import corejava.collection.assignmentset2.question1.model.Server;
-
+/* NumberFormatException
+when it is not possible to convert a string in any numeric type (float, int, etc), this exception is thrown.
+*/
 public class ServerMapper {
     public static Server map(String data){
         String[] arr = data.split(",");
-        String serverName = arr[0].trim();
-        String typesOfSoftware = arr[1].trim();
-        String applicationName = arr[2].trim();
-        Double version = Double.parseDouble(arr[3].trim());
-        Server server = new Server(serverName,typesOfSoftware,applicationName,version);
-        return server;
+       try{
+           String serverName = arr[0].trim();
+           String typesOfSoftware = arr[1].trim();
+           String applicationName = arr[2].trim();
+           Double version = Double.parseDouble(arr[3].trim());
+           Server server = new Server(serverName,typesOfSoftware,applicationName,version);
+           return server;
+
+       }catch (NumberFormatException | IndexOutOfBoundsException ex){
+           throw new RuntimeException(ex);
+       }
+//        String serverName = arr[0].trim();
+//        String typesOfSoftware = arr[1].trim();
+//        String applicationName = arr[2].trim();
+//        Double version = Double.parseDouble(arr[3].trim());
+//        Server server = new Server(serverName,typesOfSoftware,applicationName,version);
+//        return server;
     }
 }
