174cdd4db436fb16da5b567432fd023ada9e87d6
saurabh-cognologix <saurabh.singh@cognologix.com>
2022-09-27 11:05:46 +0530

    append the data in doubly linked list

---
+++ assignmentset2/question10/Main.java
@@ -0,0 +1,54 @@
+package corejava.collection.assignmentset2.question10;
+
+public class Main {
+    Node head;
+    //pushing data at last of linked list
+    public void push(int data){
+        Node newNode = new Node(data);
+        Node last = head;
+        newNode.next = null;
+        if(head == null){
+            newNode.previous= null;
+            head = newNode;
+            return;
+        }
+        while (last.next !=null){
+            last = last.next;
+        }
+        last.next = newNode;
+        newNode.previous = last;
+    }
+
+    //pop (remove value at back);
+
+
+
+    //shift (remove value at front).
+
+
+
+    //unshift (insert value at front);
+
+    //print the doubly linked list
+    public void printList(Node node){
+        Node last = null;
+        while(node != null){
+            System.out.print(node.data+"  ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.push(12);
+        main.push(56);
+        main.push(31);
+        main.push(20);
+        main.push(10);
+
+        main.printList(main.head);
+    }
+}
---
+++ assignmentset2/question10/Node.java
@@ -0,0 +1,10 @@
+package corejava.collection.assignmentset2.question10;
+
+public class Node {
+    int data;
+    Node previous;
+    Node next;
+    Node(int d){
+        data = d;
+    }
+}
