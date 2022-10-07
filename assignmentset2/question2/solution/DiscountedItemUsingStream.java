50284fc67dedb3e6d8cc7ef6f89cc47dcf8954f0
saurabh-cognologix <saurabh.singh@cognologix.com>
2022-10-07 13:47:00 +0530

    added comment

--- assignmentset2/question2/solution/DiscountedItemUsingStream.java
+++ assignmentset2/question2/solution/DiscountedItemUsingStream.java
@@ -6,6 +6,9 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class DiscountedItemUsingStream {
+    //This function is used to find each product with heighest price
+    // In key -> product Name
+    //& In Value heighest price
     public Map<String,Double> productWithHighestPrice(List<Customer> customerList){
         return customerList.stream()
                 .collect(Collectors.groupingBy(
@@ -13,10 +16,13 @@ public class DiscountedItemUsingStream {
                         Collectors.collectingAndThen(
                                 Collectors.maxBy(Comparator.comparing(Customer::getPrice)),
                                 e->e.map(Customer::getPrice).orElse((double) -1)
+                                //orElse((double) -1) =>used to remove Optional<Double>
                         )
 
                 ));
     }
+
+    //Storing customer Name in set
     public Set<String> customerListStream(List<Customer> customerList,Map<String,Double> tempMap){
         return customerList.stream()
                 .filter(
